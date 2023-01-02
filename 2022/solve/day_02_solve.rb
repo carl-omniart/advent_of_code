d3672b1e288fc73329747ef526ca16aa315ed50e
Carl Berndash Omniart <50526700+carl-omniart@users.noreply.github.com>
2023-01-02 12:36:00 -0800

    Add files via upload

--- 2022/solve/day_02_solve.rb
+++ 2022/solve/day_02_solve.rb
@@ -1,15 +1,15 @@
 require 'day_02'
 
 def solution_to_part_one
-  input          = File.read 'data/day_02_input.txt'
-  strategy_guide = RockPaperScissors.parse input
-  strategy_guide.my_score
+  input = File.read 'data/day_02_input.txt'
+  guide = RockPaperScissors::StrategyGuide.parse input
+  guide.simulate_game.score.last
 end
 
 def solution_to_part_two
-  input          = File.read 'data/day_02_input.txt'
-  strategy_guide = RockPaperScissors.parse input, updated: true  
-  strategy_guide.my_score
+  input = File.read 'data/day_02_input.txt'
+  guide = RockPaperScissors::UpdatedStrategyGuide.parse input
+  guide.simulate_game.score.last
 end
 
 puts "Day 2: Rock Paper Scissors"
