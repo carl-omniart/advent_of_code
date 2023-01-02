cf000b8c6f2486466244806e093848e24a7114b8
Carl Berndash Omniart <50526700+carl-omniart@users.noreply.github.com>
2023-01-02 00:36:48 -0800

    Add files via upload

--- 2022/solve/day_01_solve.rb
+++ 2022/solve/day_01_solve.rb
@@ -2,13 +2,13 @@ require 'day_01'
 
 def solution_to_part_one
   input = File.read 'data/day_01_input.txt'
-  elves = CalorieCounting.parse input
+  elves = CalorieCounting.get_elves input
   elves.map(&:total_calories).max
 end
 
 def solution_to_part_two
   input = File.read 'data/day_01_input.txt'
-  elves = CalorieCounting.parse input
+  elves = CalorieCounting.get_elves input
   elves.map(&:total_calories).max(3).sum
 end
 
