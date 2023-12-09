80a883439378049714ad93bdbdf105a7c523b48e
Carl Berndash Omniart <50526700+carl-omniart@users.noreply.github.com>
2023-12-09 14:25:30 -0800

    Add files via upload

--- 2023/solve/day_08_solve.rb
+++ 2023/solve/day_08_solve.rb
@@ -3,15 +3,15 @@ require 'day_08'
 def solution_to_part_one
   input = File.read 'data/day_08_input.txt'
   map   = HauntedWasteland::Map.parse input
-  map.steps_to_zzz
+  map.steps_between :AAA, :ZZZ
 end
 
 def solution_to_part_two
   input = File.read 'data/day_08_input.txt'
   map   = HauntedWasteland::Map.parse input
-  map.ghost_steps_to_z
+  map.ghost_syzygy
 end
 
-puts "Day 7: Haunted Wasteland"
+puts "Day 8: Haunted Wasteland"
 puts "  1. #{solution_to_part_one}"
 puts "  2. #{solution_to_part_two}"
